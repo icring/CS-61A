f655e111f8e3e10f0c67afb86203989947e67413
Sony Theakanath <stheakanath@gmail.com>
2014-10-13 11:20:10 -0700

    Added initial ants template.

--- homework/hw5.py
+++ homework/hw5.py
@@ -31,8 +31,8 @@ def shuffle(cards):
     arr1, arr2 = cards[len(cards)//2:], cards[:len(cards)//2]
     new = []
     for x in range(len(arr1)):
-    	new.append(arr2[x])
-    	new.append(arr1[x])
+        new.append(arr2[x])
+        new.append(arr1[x])
     return new
 
 def trade(first, second):
@@ -63,6 +63,23 @@ def trade(first, second):
     [4, 3, 1, 4, 1]
     """
     "*** YOUR CODE HERE ***"
+    def return_sum(arr):
+        sum, total = [], 0
+        for x in range(len(arr)):
+            total += arr[x]
+            sum.append(total)
+        return sum
+    first_sum, second_sum = return_sum(first), return_sum(second)
+    index, index2, m, n =  [i for i, item in enumerate(first_sum) if item in set(second_sum)], [i for i, item in enumerate(second_sum) if item in set(first_sum)], 1, 1
+    if len(index) > 0:
+        m, n = index[0]+1, index2[0]+1
+    equal_prefix = lambda: sum(first[:m]) == sum(second[:n])
+    if equal_prefix():
+        first[:m], second[:n] = second[:n], first[:m]
+        return 'Deal!'
+    else:
+        return 'No deal!'
+
 
 ################################
 # Linked list data abstraction #
@@ -128,6 +145,13 @@ def has_prefix(s, prefix):
     False
     """
     "*** YOUR CODE HERE ***"
+    if prefix == empty:
+        return True
+    if s == empty:
+        return False
+    if first(s) == first(prefix):
+        return has_prefix(rest(s), rest(prefix))
+    return False
 
 def has_sublist(s, sublist):
     """Returns whether sublist appears somewhere within linked list s.
@@ -152,6 +176,14 @@ def has_sublist(s, sublist):
     False
     """
     "*** YOUR CODE HERE ***"
+    if sublist == empty:
+        return True
+    if s == empty:
+        return False
+    if has_prefix(s, sublist):
+        return True
+    return has_sublist(rest(s), sublist)
+
 
 def has_61A_gene(dna):
     """Returns whether linked list dna contains the CATCAT gene.
@@ -172,6 +204,8 @@ def has_61A_gene(dna):
     False
     """
     "*** YOUR CODE HERE ***"
+    catcat = link('C', link('A', link('T', link('C', link('A', link('T', empty))))))
+    return has_sublist(dna, catcat)
 
 def make_withdraw(balance, password):
     """Return a password-protected withdraw function.
@@ -197,6 +231,22 @@ def make_withdraw(balance, password):
     "Your account is locked. Attempts: ['hwat', 'a', 'n00b']"
     """
     "*** YOUR CODE HERE ***"
+    attempts, times = [], 0
+    def withdraw(amount, attempted_password):
+        nonlocal balance, attempts, times
+        if times == 3:
+            return "Your account is locked. Attempts: " + str(attempts)
+        if attempted_password != password:
+            attempts, times = attempts + [attempted_password], times + 1
+            if times > 3:
+                return "Your account is locked. Attempts: " + str(attempts)
+            return "Incorrect password"
+        if amount > balance:
+           return 'Insufficient funds'
+        balance = balance - amount
+        return balance
+    return withdraw
+    
 
 def make_joint(withdraw, old_password, new_password):
     """Return a password-protected withdraw function that has joint access to
@@ -237,157 +287,164 @@ def make_joint(withdraw, old_password, new_password):
     "Your account is locked. Attempts: ['my', 'secret', 'password']"
     """
     "*** YOUR CODE HERE ***"
-
+    err = withdraw(0, old_password)
+    if type(err) == str:
+        return err
+    def attempt(amount, attempted_password):
+        if attempted_password == new_password:
+            return withdraw(amount, old_password)
+        return withdraw(amount, attempted_password)
+    return attempt
 
 ######################
 # Challenge problems #
 ######################
 
-def triangle_area(a, b, h):
-    """Connect a, b, and h so that a is the area of a triangle with base b and
-    height h.
+# def triangle_area(a, b, h):
+#     """Connect a, b, and h so that a is the area of a triangle with base b and
+#     height h.
 
-    >>> a, b, h = [connector(n) for n in ('area', 'base', 'height')]
-    >>> triangle_area(a, b, h)
-    >>> a['set_val']('user', 75.0)
-    area = 75.0
-    >>> b['set_val']('user', 15.0)
-    base = 15.0
-    height = 10.0
-    """
-    "*** YOUR CODE HERE ***"
+#     >>> a, b, h = [connector(n) for n in ('area', 'base', 'height')]
+#     >>> triangle_area(a, b, h)
+#     >>> a['set_val']('user', 75.0)
+#     area = 75.0
+#     >>> b['set_val']('user', 15.0)
+#     base = 15.0
+#     height = 10.0
+#     """
+#     "*** YOUR CODE HERE ***"
 
-def squarer(a, b):
-    """The constraint that a*a=b.
+# def squarer(a, b):
+#     """The constraint that a*a=b.
 
-    >>> x, y = connector('X'), connector('Y')
-    >>> s = squarer(x, y)
-    >>> x['set_val']('user', 10)
-    X = 10
-    Y = 100
-    >>> x['forget']('user')
-    X is forgotten
-    Y is forgotten
-    >>> y['set_val']('user', 16)
-    Y = 16
-    X = 4.0
-    """
-    "*** YOUR CODE HERE ***"
+#     >>> x, y = connector('X'), connector('Y')
+#     >>> s = squarer(x, y)
+#     >>> x['set_val']('user', 10)
+#     X = 10
+#     Y = 100
+#     >>> x['forget']('user')
+#     X is forgotten
+#     Y is forgotten
+#     >>> y['set_val']('user', 16)
+#     Y = 16
+#     X = 4.0
+#     """
+#     "*** YOUR CODE HERE ***"
 
-def pythagorean(a, b, c):
-    """Connect a, b, and c into a network for the Pythagorean theorem:
-    a*a + b*b = c*c
+# def pythagorean(a, b, c):
+#     """Connect a, b, and c into a network for the Pythagorean theorem:
+#     a*a + b*b = c*c
 
-    >>> a, b, c = [connector(name) for name in ('A', 'B', 'C')]
-    >>> pythagorean(a, b, c)
-    >>> a['set_val']('user', 5)
-    A = 5
-    >>> c['set_val']('user', 13)
-    C = 13
-    B = 12.0
-    """
-    "*** YOUR CODE HERE ***"
+#     >>> a, b, c = [connector(name) for name in ('A', 'B', 'C')]
+#     >>> pythagorean(a, b, c)
+#     >>> a['set_val']('user', 5)
+#     A = 5
+#     >>> c['set_val']('user', 13)
+#     C = 13
+#     B = 12.0
+#     """
+#     "*** YOUR CODE HERE ***"
 
-def connector(name=None):
-    """A connector between constraints.
+# def connector(name=None):
+#     """A connector between constraints.
 
-    >>> celsius = connector('Celsius')
-    >>> fahrenheit = connector('Fahrenheit')
-    >>> converter(celsius, fahrenheit)
+#     >>> celsius = connector('Celsius')
+#     >>> fahrenheit = connector('Fahrenheit')
+#     >>> converter(celsius, fahrenheit)
 
-    >>> celsius['set_val']('user', 25)
-    Celsius = 25
-    Fahrenheit = 77.0
+#     >>> celsius['set_val']('user', 25)
+#     Celsius = 25
+#     Fahrenheit = 77.0
 
-    >>> fahrenheit['set_val']('user', 212)
-    Contradiction detected: 77.0 vs 212
+#     >>> fahrenheit['set_val']('user', 212)
+#     Contradiction detected: 77.0 vs 212
 
-    >>> celsius['forget']('user')
-    Celsius is forgotten
-    Fahrenheit is forgotten
+#     >>> celsius['forget']('user')
+#     Celsius is forgotten
+#     Fahrenheit is forgotten
 
-    >>> fahrenheit['set_val']('user', 212)
-    Fahrenheit = 212
-    Celsius = 100.0
-    """
-    informant = None  # The source of the current val
-    constraints = []  # A list of connected constraints
+#     >>> fahrenheit['set_val']('user', 212)
+#     Fahrenheit = 212
+#     Celsius = 100.0
+#     """
+#     informant = None  # The source of the current val
+#     constraints = []  # A list of connected constraints
 
-    def set_value(source, value):
-        nonlocal informant
-        val = connector['val']
-        if val is None:
-            informant, connector['val'] = source, value
-            if name is not None:
-                print(name, '=', value)
-            inform_all_except(source, 'new_val', constraints)
-        else:
-            if val != value:
-                print('Contradiction detected:', val, 'vs', value)
+#     def set_value(source, value):
+#         nonlocal informant
+#         val = connector['val']
+#         if val is None:
+#             informant, connector['val'] = source, value
+#             if name is not None:
+#                 print(name, '=', value)
+#             inform_all_except(source, 'new_val', constraints)
+#         else:
+#             if val != value:
+#                 print('Contradiction detected:', val, 'vs', value)
 
-    def forget_value(source):
-        nonlocal informant
-        if informant == source:
-            informant, connector['val'] = None, None
-            if name is not None:
-                print(name, 'is forgotten')
-            inform_all_except(source, 'forget', constraints)
-    connector = {'val': None,
-                 'set_val': set_value,
-                 'forget': forget_value,
-                 'has_val': lambda: connector['val'] is not None,
-                 'connect': lambda source: constraints.append(source)}
+#     def forget_value(source):
+#         nonlocal informant
+#         if informant == source:
+#             informant, connector['val'] = None, None
+#             if name is not None:
+#                 print(name, 'is forgotten')
+#             inform_all_except(source, 'forget', constraints)
+#     connector = {'val': None,
+#                  'set_val': set_value,
+#                  'forget': forget_value,
+#                  'has_val': lambda: connector['val'] is not None,
+#                  'connect': lambda source: constraints.append(source)}
 
-    return connector
+#     return connector
 
-def inform_all_except(source, message, constraints):
-    """Inform all constraints of the message, except source."""
-    for c in constraints:
-        if c != source:
-            c[message]()
+# def inform_all_except(source, message, constraints):
+#     """Inform all constraints of the message, except source."""
+#     for c in constraints:
+#         if c != source:
+#             c[message]()
 
-def ternary_constraint(a, b, c, ab, ca, cb):
-    """The constraint that ab(a,b)=c and ca(c,a)=b and cb(c,b)=a."""
-    def new_value():
-        av, bv, cv = [connector['has_val']() for connector in (a, b, c)]
-        if av and bv:
-            c['set_val'](constraint, ab(a['val'], b['val']))
-        elif av and cv:
-            b['set_val'](constraint, ca(c['val'], a['val']))
-        elif bv and cv:
-            a['set_val'](constraint, cb(c['val'], b['val']))
-    def forget_value():
-        for connector in (a, b, c):
-            connector['forget'](constraint)
-    constraint = {'new_val': new_value, 'forget': forget_value}
-    for connector in (a, b, c):
-        connector['connect'](constraint)
-    return constraint
+# def ternary_constraint(a, b, c, ab, ca, cb):
+#     """The constraint that ab(a,b)=c and ca(c,a)=b and cb(c,b)=a."""
+#     def new_value():
+#         av, bv, cv = [connector['has_val']() for connector in (a, b, c)]
+#         if av and bv:
+#             c['set_val'](constraint, ab(a['val'], b['val']))
+#         elif av and cv:
+#             b['set_val'](constraint, ca(c['val'], a['val']))
+#         elif bv and cv:
+#             a['set_val'](constraint, cb(c['val'], b['val']))
+#     def forget_value():
+#         for connector in (a, b, c):
+#             connector['forget'](constraint)
+#     constraint = {'new_val': new_value, 'forget': forget_value}
+#     for connector in (a, b, c):
+#         connector['connect'](constraint)
+#     return constraint
 
-from operator import add, sub, mul, truediv
+# from operator import add, sub, mul, truediv
 
-def adder(a, b, c):
-    """The constraint that a + b = c."""
-    return ternary_constraint(a, b, c, add, sub, sub)
+# def adder(a, b, c):
+#     """The constraint that a + b = c."""
+#     return ternary_constraint(a, b, c, add, sub, sub)
 
-def multiplier(a, b, c):
-    """The constraint that a * b = c."""
-    return ternary_constraint(a, b, c, mul, truediv, truediv)
+# def multiplier(a, b, c):
+#     """The constraint that a * b = c."""
+#     return ternary_constraint(a, b, c, mul, truediv, truediv)
 
-def constant(connector, value):
-    """The constraint that connector = value."""
-    constraint = {}
-    connector['set_val'](constraint, value)
-    return constraint
+# def constant(connector, value):
+#     """The constraint that connector = value."""
+#     constraint = {}
+#     connector['set_val'](constraint, value)
+#     return constraint
 
-def converter(c, f):
-    """Connect c to f to convert from Celsius to Fahrenheit."""
-    u, v, w, x, y = [connector() for _ in range(5)]
-    multiplier(c, w, u)
-    multiplier(v, x, u)
-    adder(v, y, f)
-    constant(w, 9)
-    constant(x, 5)
-    constant(y, 32)
+# def converter(c, f):
+#     """Connect c to f to convert from Celsius to Fahrenheit."""
+#     u, v, w, x, y = [connector() for _ in range(5)]
+#     multiplier(c, w, u)
+#     multiplier(v, x, u)
+#     adder(v, y, f)
+#     constant(w, 9)
+#     constant(x, 5)
+#     constant(y, 32)
 
 
